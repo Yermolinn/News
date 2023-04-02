ef43a8dd15e2336578065b2b58079c989bf566eb
Yermolinn <111793261+Yermolinn@users.noreply.github.com>
2023-04-02 14:06:25 +0300

    push

---
+++ src/favorite.js
@@ -0,0 +1,7 @@
+import './scripts/current';
+import './scripts/mobileMenu';
+import './scripts/newsCardFromStorage';
+import './scripts/favoriteBlock';
+import './scripts/themeFavorite';
+import './scripts/themeFavoriteMobile';
+import './scripts/searchNewsfromFavoriteStorage';
---
+++ src/readPage.js
@@ -0,0 +1,7 @@
+import './scripts/current';
+import './scripts/mobileMenu';
+import './scripts/newsCardFromStorage';
+import './scripts/read';
+import './scripts/themeRead';
+import './scripts/themeReadMobile';
+import './scripts/searchNewsFromReadStorage';
